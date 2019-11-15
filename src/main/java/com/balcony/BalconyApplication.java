908be41c2a6321018e81b4948ba40567eb499c84
Sergey Povarchuk <s.povarchuk@gmail.com>
2019-11-15 21:12:38 +0200

    The projec balcony was created

---
+++ src/main/java/com/balcony/BalconyApplication.java
@@ -0,0 +1,13 @@
+package com.balcony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalconyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalconyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balcony/BalconyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balcony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalconyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
