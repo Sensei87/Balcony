14813ada8fe624e02ac60f70973b9767173bc8c2
Sergey Povarchuk <s.povarchuk@gmail.com>
2019-11-18 17:12:23 +0200

    add new classes and method

--- src/main/java/com/balcony/controllers/MainController.java
+++ src/main/java/com/balcony/controllers/MainController.java
@@ -1,10 +1,16 @@
 package com.balcony.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by ASUS on 11/15/2019.
  */
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
--- src/test/java/com/balcony/BalconyApplicationTests.java
+++ src/test/java/com/balcony/BalconyApplicationTests.java
@@ -1,6 +1,5 @@
 package com.balcony;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
